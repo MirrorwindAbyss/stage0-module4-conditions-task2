0d10c9a7a335f18cd933d413d8a963b9fdcfa8f5
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-10 23:48:38 +0500

    Done!

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if(first>second){
+            System.out.println(first);
+        }
+        if(second>first){
+            System.out.println(second);
+        }
+        else{
+            System.out.println(first);
+            System.out.println(second);
+        }
     }
 }
